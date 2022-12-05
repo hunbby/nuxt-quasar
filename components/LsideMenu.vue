71add27957af68e6e41f2353f52c1bffdc7a47d1
91305119 <>
2022-12-05 10:49:22 +0900

    레이아웃 변경

--- components/LsideMenu.vue
+++ components/LsideMenu.vue
@@ -1,5 +1,11 @@
 <template>
-  <q-scroll-area class="fit">
+  <q-scroll-area
+    style="
+      height: calc(100% - 150px);
+      margin-top: 150px;
+      border-right: 1px solid #ddd;
+    "
+  >
     <q-list>
       <template v-for="(menuItem, index) in menuList" :key="index">
         <q-item
@@ -19,6 +25,19 @@
       </template>
     </q-list>
   </q-scroll-area>
+  <q-img
+    class="absolute-top"
+    src="https://cdn.quasar.dev/img/material.png"
+    style="height: 150px"
+  >
+    <div class="absolute-bottom bg-transparent">
+      <q-avatar size="56px" class="q-mb-sm">
+        <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
+      </q-avatar>
+      <div class="text-weight-bold">Razvan Stoenescu</div>
+      <div>@rstoenescu</div>
+    </div>
+  </q-img>
 </template>
 
 <script setup lang="ts">
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,24 +1,17 @@
 <template>
-  <q-layout view="hHr lpR fFf">
-    <q-header elevated class="bg-primary text-white" height-hint="98">
+  <q-layout view="lHh lpR fFf">
+
+    <q-header elevated class="bg-primary text-white">
       <q-toolbar>
         <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
 
         <q-toolbar-title>
           <q-avatar>
-            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg" />
+            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
           </q-avatar>
           Title
         </q-toolbar-title>
-
-        <q-btn dense flat round icon="menu" @click="toggleRightDrawer" />
       </q-toolbar>
-
-      <!-- <q-tabs align="left">
-        <q-route-tab to="/page1" label="Page One" />
-        <q-route-tab to="/page2" label="Page Two" />
-        <q-route-tab to="/page3" label="Page Three" />
-      </q-tabs> -->
     </q-header>
 
     <q-drawer
@@ -31,19 +24,15 @@
       <LsideMenu></LsideMenu>
     </q-drawer>
 
-    <q-drawer show-if-above v-model="rightDrawerOpen" side="right" bordered>
-      <!-- drawer content -->
-    </q-drawer>
-
     <q-page-container>
-      <slot />
+      <router-view />
     </q-page-container>
 
     <q-footer elevated class="bg-grey-8 text-white">
       <q-toolbar>
         <q-toolbar-title>
           <q-avatar>
-            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg" />
+            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
           </q-avatar>
           <div>Title</div>
         </q-toolbar-title>
@@ -54,13 +43,8 @@
 
 <script setup lang="ts">
 const leftDrawerOpen = ref(false);
-const rightDrawerOpen = ref(false);
 
 const toggleLeftDrawer = () => {
   leftDrawerOpen.value = !leftDrawerOpen.value;
-};
-
-const toggleRightDrawer = () => {
-  rightDrawerOpen.value = !rightDrawerOpen.value;
 };
 </script>
