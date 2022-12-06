cba68d142842eb124f6bd9f90293866229ae9aff
91305119 <>
2022-12-06 11:20:37 +0900

    게시글 등록 화면 디자인 추가

--- pages/board/write.vue
+++ pages/board/write.vue
@@ -1,7 +1,41 @@
 <template>
   <q-page>
     <div class="q-pa-md">
-      <TuiEditor ref="editor" v-model="modelValue" height="730px"></TuiEditor>
+      <q-select
+        style="padding-bottom: 5px"
+        outlined
+        v-model="model"
+        :options="options"
+        :dense="dense"
+        :options-dense="denseOpts"
+      >
+        <template v-slot:prepend>
+          <q-icon name="event" />
+        </template>
+      </q-select>
+      <q-input
+        outlined
+        v-model="titleText"
+        :dense="dense"
+        placeholder="제목을 입력해주세요"
+        style="padding-bottom: 5px"
+      >
+        <template v-slot:prepend>
+          <q-icon name="title" />
+        </template>
+      </q-input>
+      <q-input
+        style="padding-bottom: 5px"
+        outlined
+        v-model="tagText"
+        :dense="dense"
+        placeholder="태그을 입력해주세요"
+      >
+        <template v-slot:prepend>
+          <q-icon name="tag" />
+        </template>
+      </q-input>
+      <TuiEditor ref="editor" v-model="modelValue" height="590px"></TuiEditor>
       <div class="q-pa-sm row justify-between">
         <q-btn
           icon="list"
@@ -25,12 +59,18 @@
 const modelValue = ref("test");
 const editor = ref();
 const router = useRouter();
+const options = ["Vue", "Java", "JavaScript", "Spring Boot"];
+const model = ref(options[0]);
+const dense = ref(true);
+const denseOpts = ref(false);
+const titleText = ref("");
+const tagText = ref("");
 const writeProc = () => {
   const va = editor.value.getHtmlValue();
   console.log(va);
 };
 const moveList = () => {
-  router.push({path:'/board'})
+  router.push({ path: "/board" });
 };
 </script>
 <style></style>
